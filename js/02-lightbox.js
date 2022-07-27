3c8bdb4586ff9a19ac0ca5dec9cabf7d20edd76d
pavlogurgu <pavlo.gurgu@gmail.com>
2022-07-27 14:36:07 +0300

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const simpleGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+simpleGallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+
+
+
+function createGalleryItemsMarkup(images) {
+    return images.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+        }).join("");
+}
+
+function onClickLightBoxGallery(e) {
+    e.preventDefault();
+    const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+    });
+    }
+
+simpleGallery.addEventListener('click', onClickLightBoxGallery);
